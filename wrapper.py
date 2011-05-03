0dca4993eebf67c085fd32b0f8d3df19af2a2052
Tim te Beek <timtebeek@gmail.com>
2011-05-03 07:50:36 +0000

    Introduce variable for PACKAGE

--- wrapper.py
+++ wrapper.py
@@ -3,8 +3,9 @@ First command line argument is a module within the divergence package, e.g: "div
 Further arguments are passed to the named module's main method."""
 import sys
 NAME = sys.argv[1]
+PACKAGE = 'divergence'
 try:
-    MODULE = __import__(NAME, fromlist = ['divergence'])
+    MODULE = __import__(NAME, fromlist = [PACKAGE])
 except ImportError as ie:
-    print('Could not import {0} from divergence'.format(NAME))
+    print('Could not import {0} from {1}'.format(NAME, PACKAGE))
 MODULE.main(sys.argv[2:])
