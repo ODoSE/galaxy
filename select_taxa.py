de2ebb24264f2d26ec95c68cc325229af35c6fe6
Tim te Beek <timtebeek@gmail.com>
2011-05-19 13:27:50 +0000

    Implement taxa selection through Galaxy interface

---
+++ select_taxa.py
@@ -0,0 +1,4 @@
+"""Module to feed items into selection of genomes for clades."""
+
+from divergence.select_taxa import get_complete_genomes
+
--- wrapper.py
+++ wrapper.py
@@ -1,6 +1,7 @@
 """Module to generically wrap modules contained in divergence package for usage from within Galaxy.
 First command line argument is a module within the divergence package, e.g: "divergence.translate".
 Further arguments are passed to the named module's main method."""
+
 #First argument contains fully qualified name of module to be imported
 import sys
 NAME = sys.argv[1]
