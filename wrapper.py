212573219c0fda134b29084d1ee56ecb701b023d
Tim te Beek <timtebeek@gmail.com>
2011-06-15 11:19:38 +0000

    Also supply taxon-b argument to filter_orthologs (copy-paste error)

--- wrapper.py
+++ wrapper.py
@@ -12,10 +12,10 @@ except ImportError as ie:
     print('Could not import {0} from {1}'.format(NAME, PACKAGE))
 
 #Second argument contains name of logging output file to use
-import logging as log
-FILE_HANDLER = log.FileHandler(sys.argv[2], mode = 'w')
-FILE_HANDLER.setFormatter(log.Formatter())
-log.root.addHandler(FILE_HANDLER)
+import logging
+FILE_HANDLER = logging.FileHandler(sys.argv[2], mode = 'w')
+FILE_HANDLER.setFormatter(logging.Formatter())
+logging.root.addHandler(FILE_HANDLER)
 
 #Run main method within module with remaining arguments
 MODULE.main(sys.argv[3:])
